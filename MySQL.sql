a7148c63adce0d65545688e243c7067c16305e68
JPG-Consulting <opensource@jpg-consulting.es>
2018-08-10 21:26:31 +0200

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -1,4 +1,12 @@
-CREATE TABLE IF NOT EXISTS invoice_id (
+---
+--- Drops
+---
+DROP TABLE invoice_numbers;
+
+---
+--- Invoice numbers
+---
+CREATE TABLE IF NOT EXISTS invoice_numbers (
     company_id INT unsigned not null COMMENT 'The company registered with this invoice.',
     invoice_prefix char(5) NOT NULL COMMENT 'The invoice series prefix',
     invoice_number INT UNSIGNED NOT NULL COMMENT 'The last invoice number for the serie',
@@ -15,16 +23,16 @@ COMMENT='The last invoice IDs per series' ;
 DROP PROCEDURE if exists spInvoiceInsert;
 
 CREATE PROCEDURE spInvoiceInsert(in companyId int unsigned, invoicePrefix char(5))
-begin
+BEGIN
 	
-	declare myInvoiceId int unsigned;
+	declare myInvoiceNumber int unsigned;
 	
-	select invoice_number into myInvoiceId from invoice_id where company_id=companyId and invoice_prefix=invoicePrefix;
+	SELECT invoice_number INTO myInvoiceNumber FROM invoice_numbers WHERE company_id=companyId AND invoice_prefix=invoicePrefix;
 	
-	set myInvoiceId = IFNULL(myInvoiceId, 0);
+	SET myInvoiceId = IFNULL(myInvoiceNumber, 0);
 
-	set myInvoiceId = myInvoiceId + 1;
+	SET myInvoiceNumber = myInvoiceNumber + 1;
 
-	INSERT INTO invoice_id (company_id, invoice_prefix, invoice_number) VALUES(companyId, invoicePrefix, myInvoiceId) ON DUPLICATE KEY UPDATE invoice_number=myInvoiceId;
+	INSERT INTO invoice_numbers (company_id, invoice_prefix, invoice_number) VALUES(companyId, invoicePrefix, myInvoiceNumber) ON DUPLICATE KEY UPDATE invoice_number=myInvoiceNumber;
 
-end
+END

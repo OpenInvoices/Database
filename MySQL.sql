1f706ae538a4f1f4c76105b7eff4184f7836bc53
JPG-Consulting <opensource@jpg-consulting.es>
2018-08-10 21:03:08 +0200

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS invoice_id (
     company_id INT unsigned not null COMMENT 'The company registered with this invoice.',
-	invoice_prefix char(5) NOT NULL COMMENT 'The invoice series prefix',
-	invoice_number INT UNSIGNED NOT NULL COMMENT 'The last invoice number for the serie',
-	CONSTRAINT invoice_id_pk PRIMARY KEY (company_id, invoice_prefix,invoice_number)
+    invoice_prefix char(5) NOT NULL COMMENT 'The invoice series prefix',
+    invoice_number INT UNSIGNED NOT NULL COMMENT 'The last invoice number for the serie',
+    CONSTRAINT invoice_id_pk PRIMARY KEY (company_id, invoice_prefix,invoice_number)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8
